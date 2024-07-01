3fb468809cb4ae3bdaafdd0039d1d360ddfe979e
npraneetha005 <npraneetha2005@gmail.com>
2024-07-01 15:55:02 +0530

    Initial commit: Added project files.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // scripts/script.js
+
+// Event listener to change navigation bar background color on scroll
 window.addEventListener('scroll', function() {
     const navbar = document.getElementById('navbar');
     if (window.scrollY > 50) {
-        navbar.style.backgroundColor = '#555';
+        navbar.style.backgroundColor = '#555'; // Change background color when scrolled down
     } else {
-        navbar.style.backgroundColor = '#333';
+        navbar.style.backgroundColor = '#333'; // Default background color when at the top
     }
 });
